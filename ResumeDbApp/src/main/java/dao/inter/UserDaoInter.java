afb80d0ea7cf302ed97bc1bfe53c339e4d993829
Nihad <esgerlinihad7@gmail.com>
2022-09-24 00:28:28 +0400

    Resume Database Application

---
+++ ResumeDbApp/src/main/java/dao/inter/UserDaoInter.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dao.inter;
+
+import entity.User;
+
+import java.util.List;
+
+public interface UserDaoInter {
+    public List<User> getAll(String name,String surname,Integer nationalityId);
+    public User getById(int id);
+    public boolean updateUser(User u);
+    public boolean addUser(User u);
+    public  boolean removeUser(int id);
+}
+
---
+++ ResumeDbApp/src/main/java/main/Main.java
@@ -0,0 +1,23 @@
+package main;
+
+import dao.inter.CountryDaoInter;
+import dao.inter.SkillDaoInter;
+import entity.Skill;
+
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+//        EmploymentHistoryDaoInter employmentHistoryDao =Contextnew.intstanceEmploymentHistoryDao();
+//        System.out.println(employmentHistoryDao.getAllEmploymentHistoryByUserId(6));
+//UserDaoInter userdao=Contextnew.intstanceUserDao();
+//        System.out.println(userdao.getById(6));
+//CountryDaoInter countrydao=Contextnew.instanceCountryDao();
+//        System.out.println(countrydao.getAll());
+//SkillDaoInter skillDao=Contextnew.instanceSkillDao();
+//        System.out.println(skillDao.insertSkill(new Skill( 20,"React")));
+    }
+//UserSkillDaoInter userSkillDaoInter=Contextnew.instanceUserSkillDao();
+//
+
+}
